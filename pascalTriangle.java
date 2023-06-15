a34b556edaef66b19e9de80167e4d5b249415746
Vinay Sharma <vinayiet435@gmail.com>
2023-06-15 10:55:00 +0530

    added new question

---
+++ pascalTriangle.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class pascalTriangle {
+
+    static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][] Pascal(int n) {
+
+        int[][] ans = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+
+            ans[i] = new int[i + 1];
+            ans[i][0] = ans[i][i] = 1;
+
+            for (int j = 1; j < i; j++) {
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int[][] ans = Pascal(n);
+        printArray(ans);
+    }
+}
